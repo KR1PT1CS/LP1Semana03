949397daa4ed6c560a4ad3dcff6b462bc244e7bc
KR1PT1CS <mogos2625727@gmail.com>
2025-03-07 14:04:21 +0000

    Added TrapAnalyzer project, along with player gear and trap types.cs

---
+++ TrapAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrapAnalyzer
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
